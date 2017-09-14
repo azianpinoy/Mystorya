776afa541b42d5bdc40e7519c75dc455a3638d35
brhelm <brhelm77@gmail.com>
2017-09-14 11:48:14 -0700

    public folder view html added

--- public/js/new.js
+++ public/js/new.js
@@ -18,6 +18,22 @@ $( document ).ready(function() {
 	getUserStories(userID);
 });
 
+//handles opening a story
+
+/*$(".bookImage").on("click", function(){
+  console.log("Click worked");
+  var storyID = this.attr("id");
+  localStorage.setItem("storyIDCookie", storyID);
+  console.log(storyID);
+})*/
+
+$(".overlay").on("click", function(){
+    var storyID = $(this).siblings(".bookImage").attr("id");
+    localStorage.setItem("storyIDCookie", storyID);
+    console.log(storyID);  
+  };
+});
+
 function getStoryData(id){
 	$.get("/api/user/" + id, function(data) {
       console.log(data);
@@ -36,18 +52,30 @@ function getStoryData(id){
     });
 }
 
-//handles opening a story
+//edit story
+function postStoryInput(){
+  event.preventDefault();
 
-/*$(".bookImage").on("click", function(){
-	console.log("Click worked");
-	var storyID = this.attr("id");
-	localStorage.setItem("storyIDCookie", storyID);
-  console.log(storyID);
-})*/
-
-$(".overlay").on("click", function(){
-    var storyID = $(this).siblings(".bookImage").attr("id");
-    localStorage.setItem("storyIDCookie", storyID);
-    console.log(storyID);  
+  var userInput = {
+    title: data.title,
+    body: data.body,
+    image: data.image,
+    video: data.video,
   };
-});
+
+      var title = data.title;
+      var body = data.body;
+      var image = data.image;
+      var video = data.video;
+      var userID = data.userID
+
+
+  if (!title || !body){
+    console.log("*Title and body are required");
+    return;
+  };
+  
+  $.post("/api/story", storyInput).then(
+        console.log("Story Input added");
+  )  
+}
